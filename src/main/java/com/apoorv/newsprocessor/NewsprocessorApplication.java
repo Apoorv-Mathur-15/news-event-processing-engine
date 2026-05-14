9dee280e4b735c37e85385f3d7a19f3d31b62280
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2026-05-14 16:53:36 +0530

    Configure application startup logging

---
+++ src/main/java/com/apoorv/newsprocessor/NewsprocessorApplication.java
@@ -0,0 +1,21 @@
+package com.apoorv.newsprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class NewsprocessorApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(NewsprocessorApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("Starting News Event Processing Engine application");
+
+		SpringApplication.run(NewsprocessorApplication.class, args);
+
+		logger.info("Application started successfully");
+	}
+
+}
