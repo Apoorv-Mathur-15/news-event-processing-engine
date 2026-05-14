0fdd66efb8bf3fa3c8bc64ec93f649e9e7ebd651
Apoorv-Mathur-15 <mathur.apoorv367@gmail.com>
2026-05-14 17:19:30 +0530

    Configure application startup logging

--- src/main/java/com/apoorv/newsprocessor/NewsprocessorApplication.java
+++ src/main/java/com/apoorv/newsprocessor/NewsprocessorApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class NewsprocessorApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(NewsprocessorApplication.class);
--- src/test/java/com/apoorv/newsprocessor/NewsprocessorApplicationTests.java
+++ src/test/java/com/apoorv/newsprocessor/NewsprocessorApplicationTests.java
@@ -1,4 +1,4 @@
-package com;
+package com.apoorv.newsprocessor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
